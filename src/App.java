b598ad9d29418d50e88d93d074600351dafcb992
harikrishna112 <harikrishna.kasagoni112@gmail.com>
2024-12-24 09:04:15 +0000

    first triangle code commmit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,17 @@
 public class App {
+
+  private static void printTriangle(Object[] arr){
+    for(int row=arr.length-1;row >=0 ;row--){
+      for(int col=0;col<row;col++){
+        System.out.print(arr[col]);
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
   
   public static void main(String[] args) {
-    
+    Integer[]  arr =  {12,23,34,45,56};
+    printTriangle(arr);
   }
 }
