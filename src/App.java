08a6893bb12ec1fefda6faf51618901978faded7
harikrishna112 <harikrishna.kasagoni112@gmail.com>
2024-12-24 11:08:32 +0000

    reverse Array Elemnts

--- src/App.java
+++ src/App.java
@@ -22,11 +22,26 @@ public class App {
     }
     return result;
   }
+//reverse the elements from the original arr
+  private static Object[] reverseArray(Object[] arr){
+    for(int i=0;i < arr.length / 2;i++){
+      Object temp = (Object) arr[i];
+      arr[i] = arr[arr.length-1-i];
+      arr[arr.length-i-1] = temp;
+    }
+    return arr;
+  }
   
   public static void main(String[] args) {
     Object[]  arr = new Integer[] {12,23,34,45,56};
+    
     printTriangle(arr);
     arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
+    arr = reverseArray(arr);
+    System.out.println(Arrays.toString(arr));
+    arr = new String[] { "HARI", "KRISHNA", "RESHMA" };
+    arr = reverseArray(arr);
     System.out.println(Arrays.toString(arr));
   }
 }
